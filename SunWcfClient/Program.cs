53134870d722c9b54f77892e954aec6d536dd5d1
孙振勇 <sunzhenyong@beisen.com>
2018-02-10 15:06:05 +0800

    server

---
+++ SunWcfClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunWcfClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SunWcfClient.ServiceReference1.ISunCalculateClient tc = new ServiceReference1.ISunCalculateClient();
+            var ddd = tc.DoWork("调用wcf服务成功了!");
+        }
+    }
+}
---
+++ SunWcfService/IISunCalculate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SunWcfService
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的接口名“IISunCalculate”。
+    [ServiceContract]
+    public interface IISunCalculate
+    {
+        [OperationContract]
+        string DoWork(string value);
+    }
+}
