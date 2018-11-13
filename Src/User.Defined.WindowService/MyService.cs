94abe40b30b49b3d0d8f22b35a8b0b85c6fbf1d3
sunzhenyong <6795669@qq.com>
2018-11-13 15:13:05 +0800

    complete Wcf Task

--- Src/User.Defined.WindowService/MyService.cs
+++ Src/User.Defined.WindowService/MyService.cs
@@ -8,10 +8,15 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using SimpleService;
+using Newtonsoft.Json;
+
 namespace SunService
 {
     public partial class MyService : ServiceBase
     {
+        ServiceHost host = new ServiceHost(typeof(HelloService));
         public MyService()
         {
             InitializeComponent();
@@ -20,19 +25,43 @@ namespace SunService
 
         protected override void OnStart(string[] args)
         {
-            using (FileStream stream = new FileStream(filePath, FileMode.Append))
-            using (StreamWriter writer = new StreamWriter(stream))
+            try
             {
-                writer.WriteLine($"{DateTime.Now},服务启动！");
+                using (FileStream stream = new FileStream(filePath, FileMode.Append))
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.WriteLine($"{DateTime.Now},服务启动前！");
+                }
+                host.Open();
+                using (FileStream stream = new FileStream(filePath, FileMode.Append))
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.WriteLine($"{DateTime.Now},服务启动后！");
+                }
+            }
+            catch (Exception ex)
+            {
+                using (FileStream stream = new FileStream(filePath, FileMode.Append))
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.WriteLine($"{DateTime.Now};ErrorMsg:{JsonConvert.SerializeObject(ex)},服务启动异常！");
+                }
             }
         }
 
         protected override void OnStop()
         {
-            using (FileStream stream = new FileStream(filePath, FileMode.Append))
-            using (StreamWriter writer = new StreamWriter(stream))
+            try
             {
-                writer.WriteLine($"{DateTime.Now},服务停止！");
+                host.Close();
+            }
+            catch (Exception ex)
+            {
+                using (FileStream stream = new FileStream(filePath, FileMode.Append))
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.WriteLine($"{DateTime.Now};ErrorMsg:{JsonConvert.SerializeObject(ex)},服务停止异常！");
+                }
             }
         }
     }
